090af11ce2325655ced0d0a44f9e6f8978d7df50
Marek929Z <117855155+Marek929Z@users.noreply.github.com>
2022-11-12 09:39:59 +0100

    Update sendDataForm.js

--- WebstormProjects/AWT-project-Zboray/public/js/sendDataForm.js
+++ WebstormProjects/AWT-project-Zboray/public/js/sendDataForm.js
@@ -6,13 +6,16 @@ let messages = [];
 if(localStorage.NewForm){
         messages=JSON.parse(localStorage.NewForm);
 }
+document.addEventListener("DOMContentLoaded", ()=>{
+        document.getElementById("sub").addEventListener("click", procesopenNewFormData);
+});
 
 const procesopenNewFormData=(event)=> {
         event.preventDefault();
         // precitanie dat a ulozenie do konstant
         const newForm = document.getElementById("openForm");
         const nopname = newForm["username"].value.trim();
-        const nopemail = newForm["email"].value.trim();
+        const nopemail = newForm["mail"].value.trim();
         const nopidzoznam = newForm["pole"].value.trim();
         const noppicture = newForm["picture"].value.trim();
         const nopbezec1 = document.getElementById("rekre").value.trim();
@@ -20,7 +23,7 @@ const procesopenNewFormData=(event)=> {
         const nopbezec3 = document.getElementById("profi").value.trim();
         const nopcheck1 = newForm["checkbox1"].value.trim();
         const nopcheck2 = newForm["checkbox2"].value.trim();
-        const nopopinion = newForm["textarea1"].value.trim();
+        const nopopinion = newForm["textarea_name"].value.trim();
 
         if(nopname==="" || nopopinion ===""){
                 window.alert("Prosim zadajte svoje meno a váš názor na stránku");
@@ -46,29 +49,7 @@ const procesopenNewFormData=(event)=> {
         newForm.reset();
 
 }
-document.addEventListener("DOMContentLoaded", ()=>{
-        document.getElementById("sub").addEventListener("click", procesopenNewFormData);
-});
+
 
 //vypis aj do console
 console.log(messages);
-
-/*
-function vlozUdaje(meno, email, nazor){
-        let vlozDiv = document.getElementById("vlozeneData");
-        let vytvorDiv = document.createElement("div");
-        let vytvorPmeno = document.createElement("p");
-        let vytvorPemail = document.createElement("p");
-        let vytvorPnazor = document.createElement("span");
-
-        vytvorPmeno.innerText = "Názor používateľa "  + meno ;
-        vytvorPemail.innerText = email;
-        vytvorPnazor.innerText = nazor;
-
-        vytvorDiv.appendChild(vytvorPmeno);
-        vytvorDiv.appendChild(vytvorPnazor);
-
-        return vlozDiv.appendChild(vytvorDiv);
-}
-
-*/
